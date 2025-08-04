b01bd0607db800848d54d5a22b882e58df2d94c9
CodeEdge <codeedge25@gmail.com>
2025-08-04 09:19:07 +0300

    Create promots.py

---
+++ promots.py
@@ -0,0 +1,10 @@
+COMPETITOR_PROMPT = """
+List the top 5 Instagram competitors in the niche: {niche}.
+For each competitor, include:
+- Username
+- Approximate follower count
+- Content style & themes
+- Engagement style
+- Unique strengths
+Present the output in a clear bullet-point or table format.
+"""
